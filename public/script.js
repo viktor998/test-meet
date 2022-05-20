d203f199ac3843b2e166adcf82e1dd40ddaff546
viktor998 <viktor.casubolo98@gmail.com>
2022-05-20 15:33:07 +0100

    test meet

---
+++ public/script.js
@@ -0,0 +1,69 @@
+const socket = io("/");
+
+const videoGrid = document.getElementById("video-grid");
+const thisUserVideo = document.createElement("video");
+thisUserVideo.muted = true;
+
+const peers = {};
+
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
+});
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: false,
+  })
+  .then((stream) => {
+    addStream(thisUserVideo, stream);
+
+    myPeer.on("call", (call) => {
+      console.log("calling");
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userStream) => {
+        addStream(video, userStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      console.log("User connected : " + userId);
+
+      connectToUserStream(userId, stream);
+    });
+  });
+
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) peers[userId].close();
+});
+
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+function addStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+
+  videoGrid.append(video);
+}
+
+function connectToUserStream(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userStream) => {
+    console.log("connect to stream");
+    addStream(video, userStream);
+  });
+
+  call.on("close", () => {
+    console.log("disconnect stream");
+    video.remove();
+  });
+
+  peers[userId] = call;
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+/* The above code is creating a server that listens on port 3000. */
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+/*  */
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+let userId;
+
+// app.get("/:room/:user", (req, res) => {
+//   userId = req.params.user;
+//   let roomId = req.params.room;
+//   res.redirect(`/${roomId}`);
+
+//   console.log(req.params);
+// });
+
+app.get("/:room", (req, res) => {
+  console.log(req.params);
+  res.render("room", {
+    roomId: req.params.room,
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    console.log(roomId, userId);
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
+
+server.listen(3023);
